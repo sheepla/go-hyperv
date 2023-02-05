ec449962fc1c7c52cbac65290bfb8f261deed1c2
sheepla <62412884+sheepla@users.noreply.github.com>
2023-02-05 15:55:00 +0900

    FIx main.go output

--- cmd/gohyperv/main.go
+++ cmd/gohyperv/main.go
@@ -15,10 +15,12 @@ func main() {
 
 	for _, vm := range *vms {
 		fmt.Printf(
-			"----------------------\n%v %v [%v]\n\tProcessors:\t%v\n\tMemory:\tusage=%v available=%v buffer=%v\n\tUptime:\t%v\n\tvSwitch:\t%v\n",
-			vm.ID,
-			vm.Name,
-			vm.State,
+			"----------------------\n%v %v [%v]\n"+
+				"\tProcessors:\t%v\n"+
+				"\tMemory:\t\tusage=%v available=%v buffer=%v\n"+
+				"\tUptime:\t\t%v\n"+
+				"\tvSwitch:\t%v\n",
+			vm.ID, vm.Name, vm.State,
 			vm.NumberOfProcessors,
 			vm.MemoryUsage, vm.MemoryAvailable, vm.AvailableMemoryBuffer,
 			vm.UpTime,
